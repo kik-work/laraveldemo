bec25a2d5d21e506a10e187ea3d51d886acc41da
kik_workgit <kik.work100@gmail.com>
2025-08-02 15:18:47 +0600

    learnt linking pages

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Hello Page</title>
+</head>
+
+<body>
+    <h1>Hello, world from KIK!</h1>
+    <a href="{{ url('/') }}">Go Back</a>
+</body>
+
+</html>
